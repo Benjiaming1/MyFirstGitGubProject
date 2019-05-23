467ee1578893a92dc6258a44f7344fa2aff2a4e0
Benjiaming1 <50933958+Benjiaming1@users.noreply.github.com>
2019-05-23 09:14:02 +0800

    增加
    增加一个新文件

---
+++ Vehicle.java
@@ -0,0 +1,39 @@
+package com.vehicle;
+
+public abstract class Vehicle {
+	//父类
+	private String brand;
+	private String vehicleId;
+	private int perSent;
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public String getVehicleId() {
+		return vehicleId;
+	}
+	public void setVehicleId(String vehicleId) {
+		this.vehicleId = vehicleId;
+	}
+	public int getPerSent() {
+		return perSent;
+	}
+	public void setPerSent(int perSent) {
+		this.perSent = perSent;
+	}
+
+	public Vehicle(){}////品牌  车牌号  日租金
+	public Vehicle(String brand, String vehicleId, int perSent) {
+		
+		this.brand = brand;
+		this.vehicleId = vehicleId;
+		this.perSent = perSent;
+	}
+	
+	//计算租金
+	public abstract float calc(int days);
+	
+	
+}
